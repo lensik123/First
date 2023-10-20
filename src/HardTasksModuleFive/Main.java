eead23fd521085024d79d689b212dd5fda4d8ce6
Turpal Baysarov <turpal.baysarov@bk.ru>
2023-10-20 14:40:23 +0300

    new branch commit

--- src/HardTasksModuleFive/Main.java
+++ src/HardTasksModuleFive/Main.java
@@ -3,8 +3,7 @@ package HardTasksModuleFive;
 public class Main {
 	public static void main (String[]args){
 
-		int count = 3;
-		count = 23;
+		System.out.println("new_branch");
 //		TicTacToe board = new TicTacToe();
 //
 //		board.newGame();
