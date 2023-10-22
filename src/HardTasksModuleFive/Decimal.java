8c68d70fdc57e5cb753d6c0e2387a254f372bd26
Turpal Baysarov <turpal.baysarov@bk.ru>
2023-10-22 11:42:06 +0300

    Decimal add

---
+++ src/HardTasksModuleFive/Decimal.java
@@ -0,0 +1,45 @@
+package HardTasksModuleFive;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Decimal {
+
+	public static void main (String[]args){
+		String numbers = "12345678";
+
+		char separator = ',';
+
+		System.out.println(DecimalFormat(numbers, separator));
+
+
+	}
+
+
+
+	static String DecimalFormat(String strNumbers, char separator){
+
+		System.out.println("");
+
+		String str = "12345678";
+		int[] arrayNumbers = new int[str.length()];
+
+		String [] strArray = new String [str.length() + str.length()/3];
+
+		Pattern pattern = Pattern.compile("(\\d)(?=(\\d{3})+(?![0-9]))");
+		Matcher matcher = pattern.matcher(str);
+		StringBuffer formattedNumber = new StringBuffer();
+
+		while (matcher.find()) {
+			matcher.appendReplacement(formattedNumber, matcher.group(1) + separator);
+		}
+
+
+		matcher.appendTail(formattedNumber);
+		return formattedNumber.toString();
+
+
+
+	}
+}
+
+
